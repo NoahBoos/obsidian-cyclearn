7187a59595ad04c6c8ed7647ff20d2c31338baca
Noah Boos <nboosgamemaker@gmail.com>
2025-08-15 00:11:47 +0200

    Feat. - Added FlaschardsModal & I_ModalOptions.

---
+++ src/modals/FlashcardsModal.ts
@@ -0,0 +1,29 @@
+import {App, Modal} from "obsidian";
+import {CreateHeader, CreateMain} from "../utils/U_CreateSemanticElements";
+import {I_ModalOptions} from "./I_ModalOptions";
+import {CreateTitle} from "../utils/U_CreateTextualElements";
+
+export abstract class FlashcardsModal extends Modal {
+    modalOptions: I_ModalOptions;
+
+    constructor(app: App, modalOptions: I_ModalOptions) {
+        super(app);
+        this.modalOptions = modalOptions;
+    }
+
+    onOpen() {
+        const {contentEl} = this;
+        contentEl.addClass("flashcards__modal");
+        const header = CreateHeader(contentEl);
+        const title = CreateTitle(header, this.modalOptions.modalTitle);
+        const main = CreateMain(contentEl);
+        this.BuildMain(main);
+    }
+
+    onClose() {
+        const {contentEl} = this;
+        contentEl.empty();
+    }
+
+    protected abstract BuildMain(parent: HTMLElement): void;
+}
---
+++ src/modals/I_ModalOptions.ts
@@ -0,0 +1,5 @@
+export interface I_ModalOptions {
+    modalTitle: string;
+    modalConfirmButtonText: string;
+    modalConfirmButtonIcon: string;
+}
