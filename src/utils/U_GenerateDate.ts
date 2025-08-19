8f080c5f209cc0d68450bb9be5f6d6f1fda0314b
Noah Boos <nboosgamemaker@gmail.com>
2025-08-19 13:22:52 +0200

    Feat. - Added GetToday() utils.

---
+++ src/utils/U_GenerateDate.ts
@@ -0,0 +1,7 @@
+export function GetToday(): string {
+    const today = new Date();
+    const day = String(today.getDay()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    return `${day}${month}${year}`;
+}
