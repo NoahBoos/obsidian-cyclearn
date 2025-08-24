f8239af777fd5e6abd54fb1c1ceb01e8a2936d1d
Noah Boos <nboosgamemaker@gmail.com>
2025-08-24 18:33:42 +0200

    Refacto. - Now use vault#configDir as a part of the database path.

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -5,7 +5,7 @@ import Cyclearn from "../main";
 export let database: Loki;
 
 export function InitializeDatabase(cyclearn: Cyclearn) {
-    const PATH_DATABASE = `${this.app.vault.adapter.basePath}/.obsidian/plugins/obsidian-${cyclearn.manifest.id}/database.json`;
+    const PATH_DATABASE = `${this.app.vault.adapter.basePath}/${this.app.vault.configDir}/plugins/obsidian-${cyclearn.manifest.id}/database.json`;
 
     if (!fs.existsSync(PATH_DATABASE)) {
         fs.writeFileSync(PATH_DATABASE, JSON.stringify({}));
