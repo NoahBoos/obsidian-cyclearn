ee90f49236ea21d2a138e55e1ed4df487fdde37d
Noah Boos <nboosgamemaker@gmail.com>
2025-11-05 22:45:21 +0100

    Refacto. - Turned empty interface declarations into comment, in `I_CyclearnSettings`.

--- src/settings/I_CyclearnSettings.ts
+++ src/settings/I_CyclearnSettings.ts
@@ -1,7 +1,8 @@
-export interface I_CyclearnSettings {
-
-}
-
-export const DEFAULT_SETTINGS: I_CyclearnSettings = {
-
-}
+// TODO - Implement Cyclearn's settings.
+// export interface I_CyclearnSettings {
+//
+// }
+//
+// export const DEFAULT_SETTINGS: I_CyclearnSettings = {
+//
+// }
